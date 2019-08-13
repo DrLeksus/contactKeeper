9e62f4317aaa07060b4a82084ddeb0b8d1b1d676
Dr Lexus <stephan.wottka@web.de>
2019-08-13 22:57:11 +0200

    added routes, mongoDB connects

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+// https://express-validator.github.io/docs/
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route     POST api/users
 // @desc      Register a user
 // @access    Public
-router.post("/", (req, res) => res.send("Register a user"));
+router.post(
+  "/",
+  [
+    check("name", "Please add name")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a vaild email").isEmail(),
+    check("password", "Please enter a strong password").isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("all good. your data has been submitted");
+  }
+);
 
 module.exports = router;
